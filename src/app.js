878222d1d5260c644c8231b059ac4eaad1dd29c9
Ilya <ilyholu@gmail.com>
2022-09-10 15:20:35 +0300

    films section

--- src/app.js
+++ src/app.js
@@ -2,6 +2,5 @@ import 'bootstrap';
 import './scss/style.scss'
 import '@fortawesome/fontawesome-free/js/fontawesome'
 import '@fortawesome/fontawesome-free/js/solid'
-import '@fortawesome/fontawesome-free/js/regular'
-import '@fortawesome/fontawesome-free/js/brands'
+
 //import '../src/autocomplete.js';
